74bc489415a627a6ee4e5226e4db77f59bbeb0e9
y1zhou <zhou.zy.yi@gmail.com>
2026-04-10 14:12:05 +0800

    fix(helper): use old typing syntax for generics to support Python 3.10

--- src/biomodals/app/helper/internal.py
+++ src/biomodals/app/helper/internal.py
@@ -1,11 +1,19 @@
 """Internal utility scripts for other helpers."""
 
+from __future__ import annotations
+
 from collections.abc import Callable
 from functools import wraps
 from time import time
+from typing import ParamSpec, TypeVar
+
+P = ParamSpec("P")
+R = TypeVar("R")
 
 
-def timed_function[**P, R](f: Callable[P, R]) -> Callable[P, R]:
+# Note: this syntax is only supported in Python 3.12+. If we want to support Python 3.10+, we can switch to using `typing_extensions` and `typing.ParamSpec` instead of `typing.P` and `typing.R`.
+# def timed_function[**P, R](f: Callable[P, R]) -> Callable[P, R]:
+def timed_function(f: Callable[P, R]) -> Callable[P, R]:  # noqa: UP047
     """Decorator to time a function and print its execution time.
 
     Credit: https://stackoverflow.com/a/27737385

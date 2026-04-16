d7992d9db7dd572aca0ed57a3d516567e0378284
y1zhou <zhou.zy.yi@gmail.com>
2026-04-16 17:54:57 +0800

    feat(helper): smaller footprint for the image patch

--- src/biomodals/app/helper/__init__.py
+++ src/biomodals/app/helper/__init__.py
@@ -3,8 +3,19 @@
 from modal import Image
 
 
-def patch_image_for_helper(image: Image) -> Image:
-    """Patch a Modal Image to include helper dependencies."""
+def patch_image_for_helper(image: Image, copy_patch_files: bool = False) -> Image:
+    """Patch a Modal Image to include helper dependencies.
+
+    Args:
+        image: The Modal Image to patch.
+        copy_patch_files: Whether to copy patch files into the image. By default,
+            the files are added to containers on startup and are not built into
+            the actual Image, which speeds up deployment.
+            Set to `True` to copy the files into an Image layer at build time instead.
+            This can slow down iteration since it requires a rebuild of the Image
+            and any subsequent build steps whenever the included files change,
+            but it is required if you want to run additional build steps after this one.
+    """
     # This is a bit hacky, but because Modal's .add_local_python_source()
     # does not install the package, the metadata.requires call would not work
     # in the runtime, so we make sure dependencies are installed here.
@@ -18,7 +29,12 @@ def patch_image_for_helper(image: Image) -> Image:
     return (
         image.apt_install("zstd", "fd-find")
         .uv_pip_install(helper_deps)
-        .add_local_python_source("biomodals", copy=True)
+        .add_local_python_source(
+            "biomodals.app.helper",
+            "biomodals.app.constant",
+            "biomodals.app.config",
+            copy=copy_patch_files,
+        )
     )
 
 
